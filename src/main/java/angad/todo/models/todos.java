2ec0a31e00a301c8bbb33320f8678d5657880271
Angad Gill <angadgill088@gmail.com>
2025-07-13 01:52:51 +0530

    java initial

--- src/main/java/angad/todo/models/todos.java
+++ src/main/java/angad/todo/models/todos.java
@@ -1,6 +1,10 @@
 package angad.todo.models;
 
+import org.springframework.data.annotation.Id;
+
 public class todos {
+    @Id
+    private String id;
     private String name;
     private boolean status;
 
